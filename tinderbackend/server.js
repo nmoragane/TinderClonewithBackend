5e3d021a9847d2ce1408f373127b0e608ace3f42
Nayomal Lakshitha <nmoragane@gmail.com>
2020-12-24 16:06:18 +0530

    mongoose config

--- tinderbackend/server.js
+++ tinderbackend/server.js
@@ -1,13 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+
 //App config
 const app = express();
 const port = process.env.PORT || 8001
+const password = 'pKPg8e4fjZPC7Vr';
+const dbname = 'tinderdb'
+
+const connection_url = `mongodb+srv://admin:${password}@cluster0.qdyhu.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 
 //Middlewares
 
 //DB config
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
 
 //API Endpoints
 app.get('/', (req, res) => res.status(200).send("Hello genius!"))
