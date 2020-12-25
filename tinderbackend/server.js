ef0f2f4041d0e28e207cc1eda3ea78a04f6e15fd
Nayomal Lakshitha <nmoragane@gmail.com>
2020-12-25 23:37:25 +0530

    added middleware

--- tinderbackend/server.js
+++ tinderbackend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
-
+import Cards from "./dbCards.js";
+import Cors from 'cors'; 
 
 //App config
 const app = express();
@@ -11,6 +12,8 @@ const dbname = 'tinderdb'
 const connection_url = `mongodb+srv://admin:${password}@cluster0.qdyhu.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 
 //Middlewares
+app.use(express.json());
+app.use(Cors());
 
 //DB config
 mongoose.connect(connection_url, {
@@ -20,7 +23,30 @@ mongoose.connect(connection_url, {
 })
 
 //API Endpoints
-app.get('/', (req, res) => res.status(200).send("Hello genius!"))
+app.get('/', (req, res) => res.status(200).send("Hello genius!"));
+
+app.post('/tinder/cards', (req,res) => {
+    const dbCard = req.body;
+
+    Cards.create(dbCard, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    })
+});
+
+app.get('/tinder/cards', (req,res) => {
+     Cards.find((err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    })
+});
+
 
 //Listener
 app.listen(port, () => console.log('listening on port ' + port))
